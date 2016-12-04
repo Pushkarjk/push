ae32bda99b3ddaae1da61add7f3a07b7e3f9100c
Pushkarjk <pushkarjk@gmail.com>
2016-12-04 11:14:04 +0530

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt = new System.Windows.Forms.TextBox();
+            this.butt = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt
+            // 
+            this.txt.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt.Location = new System.Drawing.Point(119, 76);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(100, 20);
+            this.txt.TabIndex = 0;
+            this.txt.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // butt
+            // 
+            this.butt.Location = new System.Drawing.Point(119, 162);
+            this.butt.Name = "butt";
+            this.butt.Size = new System.Drawing.Size(75, 23);
+            this.butt.TabIndex = 1;
+            this.butt.Text = "button1";
+            this.butt.UseVisualStyleBackColor = true;
+            this.butt.Click += new System.EventHandler(this.butt_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(356, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+          
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.butt);
+            this.Controls.Add(this.txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Button butt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string conString = "Data Source=LENOVO-PC;Initial Catalog=master;Integrated Security=True";
+
+
+
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butt_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            string q = "insert into hello values ('" + txt.Text.ToString() + "','12')";
+            SqlCommand cmd = new SqlCommand(q, con);
+            cmd.ExecuteNonQuery();
+            disp();
+       }
+
+        public void disp()
+        {
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            string q = "Select * from hello";
+            SqlCommand cmd = new SqlCommand(q, con);
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+
+
+
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            disp();
+        }
+    }
+}
